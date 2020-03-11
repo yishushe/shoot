d74890478dde1de66e172438b345932d5ad3dbd3
yanglong555 <yanglong555@163.com>
2020-03-11 20:56:58 +0800

    333

--- src/main/java/cn/bdqn/photography/PhotographyApplication.java
+++ src/main/java/cn/bdqn/photography/PhotographyApplication.java
@@ -12,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 "cn.bdqn.photography.shoottheme.mapper"})
 //yang
 public class PhotographyApplication {
-    //yanglongyanglongddddddddddddddddddddddddd
+    //yanglongyanglongdddddddddddddddddddddddddddddddddddd
     public static void main(String[] args) {
         SpringApplication.run(PhotographyApplication.class, args);
     }
