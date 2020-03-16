4ef6a7ce094a393318975338b2d73a7fe3ecdcbb
hanhu0616 <203783515@qq.com>
2020-03-16 16:25:41 +0800

    页面html

--- src/main/java/cn/bdqn/photography/shootattention/controller/ShootAttentionController.java
+++ src/main/java/cn/bdqn/photography/shootattention/controller/ShootAttentionController.java
@@ -1,9 +1,8 @@
 package cn.bdqn.photography.shootattention.controller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -13,8 +12,35 @@ import org.springframework.web.bind.annotation.RestController;
  * @author jobob
  * @since 2020-03-09
  */
-@RestController
+@Controller
 @RequestMapping("/shoot_attention/shoot-attention")
 public class ShootAttentionController {
+
+    @RequestMapping("/love")
+    public String add(){
+        return "houtai/article-list";
+    }
+
+    @RequestMapping("/like")
+    public String la(){
+        return "houtai/admin-role";
+    }
+
+    @RequestMapping("/lui")
+    public String lo(){
+        return "houtai/admin-role-add";
+    }
+
+    @RequestMapping("/lik")
+    public String li(){
+        return "houtai/member-list";
+    }
+
+   /* @RequestMapping("/like")
+    public String lk(){
+        return "houtai/admin-role";
+    }*/
+
+
 
 }
